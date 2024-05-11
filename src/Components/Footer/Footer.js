0b41ae518988487c39caadf19293f3ad66f4629e
DaniYeCa <daniel.dance1912@gmail.com>
2024-05-11 11:32:37 +0200

    change

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,8 +4,8 @@ import './Footer.css';
 export const Footer = () => {
   return `
           <div class="icons">
-                <a href="https://github.com/DaniYeCa" target="_blank"><img src="public/assets/icons8-github-64.png" alt="logo github"></a>
-                <a href="https://www.linkedin.com/in/daniel-yepes-carrillo/" target="_blank"><img src="public/assets/icons8-linkedin-64.png" alt="logo linkedin"></a> 
+                <a href="https://github.com/DaniYeCa" target="_blank"><img src="./assets/icons8-github-64.png" alt="logo github"></a>
+                <a href="https://www.linkedin.com/in/daniel-yepes-carrillo/" target="_blank"><img src="./assets/icons8-linkedin-64.png" alt="logo linkedin"></a> 
           </div>
   `
 }
